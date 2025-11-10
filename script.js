841bcbe98bf68abd1d4fa889b163df0e7545892d
Rahul-Tankhiwale <153199890+Rahul-Tankhiwale@users.noreply.github.com>
2025-11-10 10:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let delay = document.getElementById("delay");
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
 
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-btn.addEventListner("submit", ()=>{
-	async function formDelay(text, delay) {
-		setTimeout(()=>{
-			await output.textContent = text;
-			await output.textContent = delay;
-		}, 1000)
-	}
-})
+btn.addEventListener("click", async () => {
+  let message = text.value;
+  let delayTime = Number(delay.value);
+
+  await wait(delayTime);
+  output.textContent = message;
+});
