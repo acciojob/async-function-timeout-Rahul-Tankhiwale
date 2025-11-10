9d4d3f663640e11a3e2d808182bcc02eba182066
Rahul-Tankhiwale <153199890+Rahul-Tankhiwale@users.noreply.github.com>
2025-11-10 10:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+
+
+btn.addEventListner("submit", ()=>{
+	async function formDelay(text, delay) {
+		setTimeout(()=>{
+			await output.textContent = text;
+			await output.textContent = delay;
+		}, 1000)
+	}
+})
